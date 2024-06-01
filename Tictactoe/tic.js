3388a9d4edaca5d6d73a329739b3bbc72034cba5
code-withshruti <shrutighadashi@gmail.com>
2024-06-01 23:38:14 +0530

    project

---
+++ Tictactoe/tic.js
@@ -0,0 +1,97 @@
+function myfunc_8() { 
+
+    if (flag == 1) { 
+
+        document.getElementById("b6").value = "X"; 
+
+        document.getElementById("b6").disabled = true; 
+
+        flag = 0; 
+
+    } 
+
+    else { 
+
+        document.getElementById("b6").value = "0"; 
+
+        document.getElementById("b6").disabled = true; 
+
+        flag = 1; 
+
+    } 
+} 
+
+  
+
+function myfunc_9() { 
+
+    if (flag == 1) { 
+
+        document.getElementById("b7").value = "X"; 
+
+        document.getElementById("b7").disabled = true; 
+
+        flag = 0; 
+
+    } 
+
+    else { 
+
+        document.getElementById("b7").value = "0"; 
+
+        document.getElementById("b7").disabled = true; 
+
+        flag = 1; 
+
+    } 
+} 
+
+  
+
+function myfunc_10() { 
+
+    if (flag == 1) { 
+
+        document.getElementById("b8").value = "X"; 
+
+        document.getElementById("b8").disabled = true; 
+
+        flag = 0; 
+
+    } 
+
+    else { 
+
+        document.getElementById("b8").value = "0"; 
+
+        document.getElementById("b8").disabled = true; 
+
+        flag = 1; 
+
+    } 
+} 
+
+  
+
+function myfunc_11() { 
+
+    if (flag == 1) { 
+
+        document.getElementById("b9").value = "X"; 
+
+        document.getElementById("b9").disabled = true; 
+
+        flag = 0; 
+
+    } 
+
+    else { 
+
+        document.getElementById("b9").value = "0"; 
+
+        document.getElementById("b9").disabled = true; 
+
+        flag = 1; 
+
+    } 
+}
